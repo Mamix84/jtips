a0c365f42b20ef8e7a1de41437df4372fed10d33
marco.marino <marco.marino@CPX-0A70BDS7738.dir.svc.accenture.com>
2019-09-22 17:16:24 +0200

    Spring XML and Annotation web app

---
+++ SpringAnnotationConfig/src/main/java/it/mamino84/example/spring/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package it.mamino84.example.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "it.mamino84")
+public class ApplicationConfig {
+
+}
---
+++ SpringXmlConfig/src/main/java/it/mamino84/example/spring/BookController.java
@@ -0,0 +1,9 @@
+package it.mamino84.example.spring;
+
+public class BookController {
+
+	public BookController() {
+		System.out.println("Book Controller Bean created by xml configuration...");
+		
+	}
+}
